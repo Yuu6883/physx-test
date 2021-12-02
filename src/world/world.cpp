6ff395a675c7628340324841edf571947aabd634
Yuu <okumura6883@gmail.com>
2021-12-02 02:34:57 -0800

    Cmake & VS config

--- src/world/world.cpp
+++ src/world/world.cpp
@@ -1,8 +1,5 @@
 #include "world.hpp"
-#include <chrono>
 #include <thread>
-
-using namespace std::chrono;
 
 static PxDefaultErrorCallback defaultErrorCallback;
 static PxDefaultAllocator defaultAllocatorCallback;
@@ -24,8 +21,10 @@ int World::init() {
     bool recordMemoryAllocations = true;
 
     pvd = PxCreatePvd(*foundation);
-    transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
-    pvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+    transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 100);
+    auto connected = pvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+
+	if (connected) printf("Connected to PVD\n");
 
     physics = PxCreatePhysics(PX_PHYSICS_VERSION, *foundation,
         PxTolerancesScale(), recordMemoryAllocations, pvd);
@@ -52,16 +51,17 @@ World::World() {
     PxSceneDesc sceneDesc(physics->getTolerancesScale());
 	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
 
-	auto bitconnect = std::thread::hardware_concurrency();
+	auto t = 4; // std::thread::hardware_concurrency();
 
-	printf("Hardware threads: %u\n", bitconnect);
+	printf("Hardware threads: %u\n", t);
 
-	dispatcher = PxDefaultCpuDispatcherCreate(bitconnect);
+	dispatcher = PxDefaultCpuDispatcherCreate(t);
 	sceneDesc.cpuDispatcher = dispatcher;
 	sceneDesc.filterShader = PxDefaultSimulationFilterShader;
 	scene = physics->createScene(sceneDesc);
 
     pvdClient = scene->getScenePvdClient();
+
 	if (pvdClient) {
 		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
 		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
@@ -99,16 +99,20 @@ void World::initScene() {
 		shape->release();
 	*/
 
-	for (int stack = 0; stack < 20; stack++) {
-		for (int i = 1; i < 20; i++) {
-			for (int j = 1; j < 20; j++) {
-				auto dynamic = PxCreateDynamic(*physics, PxTransform(PxVec3(i, stack + 0.0001f, j)), PxBoxGeometry(0.5f, 0.5f, 0.5f), *material, 10.0f);
-				dynamic->setAngularDamping(0.5f);
-				scene->addActor(*dynamic);
+	for (int x = 0; x < 1; x++) {
+		for (int stack = 0; stack < 20; stack++) {
+			for (int i = 1; i < 20; i++) {
+				for (int j = 1; j < 20; j++) {
+					auto dynamic = PxCreateDynamic(*physics, PxTransform(PxVec3(i + x * 50, stack + 0.0001f, j)), 
+						PxBoxGeometry(0.48f, 0.48f, 0.48f), *material, 10.0f);
+					dynamic->setAngularDamping(0.5f);
+					scene->addActor(*dynamic);
+				}
 			}
 		}
 	}
 
+	// Destroyer ball
 	auto dynamic = PxCreateDynamic(*physics, PxTransform(PxVec3(10, 50, 10)), PxSphereGeometry(10), *material, 10.0f);
 	dynamic->setAngularDamping(0.5f);
 	// dynamic->setLinearVelocity(PxVec3(0, -5, -10));
@@ -116,12 +120,6 @@ void World::initScene() {
 }
 
 void World::step(float dt) {
-	auto start = high_resolution_clock::now();
-
 	scene->simulate(1.f / 60);
 	scene->fetchResults(true);
-
-	auto end = high_resolution_clock::now();
-
-	printf("dt = %.2fms\n", duration_cast<microseconds>(end - start).count() / 1000.f);
 }
